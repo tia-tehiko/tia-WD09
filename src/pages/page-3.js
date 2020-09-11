f967b4120f63ffbad7fc0321a6cec7dc501237d0
Tia TeHiko <tiatehiko@gmail.com>
2020-09-11 21:09:46 +1200

    change page names

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const ThirdPage = () => (
   <Layout>
     <SEO title="Page Three" />
-    <h1>Hi from the Third page</h1>
+    <h1>Hi, I made this page</h1>
     <p>Welcome to page 3</p>
-    <p>Im really not sure what Data im meant to put into these pages? But I made same pages and they techincally have data?</p>
+    <p>Im really not sure what Data im meant to put into these pages?</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
--- src/pages/page-4.js
+++ src/pages/page-4.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const FourthPage = () => (
   <Layout>
     <SEO title="Page Three" />
-    <h1>Hi from the Fourth page</h1>
+    <h1>& this one </h1>
     <p>Welcome to page 4</p>
     <p>If this isn't right I can definitely fix it lol</p>
     <Link to="/">Go back to the homepage</Link>
